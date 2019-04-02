5ed4a6b6fedc5042d168163d40576d0181613526
Jose Masri <josemasri222@gmail.com>
2019-04-02 09:38:03 -0700

    Added a button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  text = 'Default starting text';
+  onChangeText() {
+    this.text = 'Changed';
+  }
 }
